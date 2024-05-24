00dbad943d7a57dbcd16cc87f124b17249d92530
KotDenisV <kot.denis@gmail.com>
2024-05-25 00:00:26 +0300

    25/05 0:00

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,26 @@
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
 const form = document.querySelector('.images-form');
 const imagesList = document.querySelector('.images-container');
+const request = document.querySelector('.images-form-input');
 
-form.addEventListener('submit', event => {
+
+form.addEventListener('submit', handleSubmit);
+// form.addEventListener('click', handleClick);
+
+function handleSubmit(event) {
   event.preventDefault();
+    if (request.value.trim() === '') {
+      alert('Please enter a search query');
+      return; 
+  }
 
   const query = event.target.elements.query.value.trim();
 
-  searchImage(query)
+  searchImages(query)
     .then(data => {
       const markup = imagesTemplate(data.hits);
       imagesList.innerHTML = markup;
@@ -14,27 +28,48 @@ form.addEventListener('submit', event => {
     .catch(err => {
       console.log(err);
     });
-});
+};
 
-function searchImage(query) {
+
+function searchImages(query) {
     const baseUrl = 'https://pixabay.com/api/';
     const params = new URLSearchParams({
     key: '44020863-07a417a47184dc9fb21f180f5',
     q: query,
     lang: 'en',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
   });
     const url = `${baseUrl}?${params}`;
 
-    return fetch(url).then(response => response.json());
+    return fetch(url).then(response => {
+        if(!response.ok) {
+          throw new Error(response.statusText)
+        }
+        response.json()
+    });
 }
 
 function imageTemplate(image) {
   return `<img
-  src="${image.previewURL}"
+  src="${image.webformatURL}"
   alt="image"
+  width="360"
+  height="200"
 />`;
 }
 
 function imagesTemplate(arr) {
+  if (arr.length === 0) {
+        iziToast.show({
+          position: 'topRight',
+          backgroundColor: '#EF4040',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          messageColor: '#FAFAFB',
+        });
+    }
   return arr.map(imageTemplate).join('');
 }
+
